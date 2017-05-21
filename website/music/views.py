3d89270d7c5010de22db1a54019ee38056805a5b
Pawan Moolrajani <Pawan Moolrajani>
2017-05-21 08:58:04 +0530

    adding files

---
+++ website/music/views.py
@@ -0,0 +1,30 @@
+from django.http import Http404
+from django.http import HttpResponse
+from .models import  Album
+#from django.template import loader
+from django.shortcuts import render
+"""
+def index(request):
+    all_albums=Album.objects.all()
+    html=''
+    for album in all_albums:
+        url='/music/'+str(album.id)+'/'
+        html+='<a href="'+url+'">'+album.album_title + '</a><br>'
+    return HttpResponse(html)
+"""
+def index(request):
+    all_albums=Album.objects.all()
+    #template=loader.get_template('music/index.html')
+    context={
+        'all_albums':all_albums,
+    }
+    #return HttpResponse(template.render(context,request))
+    return render(request,'music/index.html',context)
+
+def details(request,album_id):
+    #return HttpResponse("<h2>Details for Album id:" + str(album_id)+"</h2>")
+    try:
+        album=Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404("Album does not exist")
+    return render(request, 'music/detail.html', {'album':album})
