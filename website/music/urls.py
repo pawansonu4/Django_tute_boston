cec04efd68112be228a3c091c52c6ba55308da5b
pawan <pawansonu4@gmail.com>
2017-05-21 12:36:39 +0530

    http 404 shortcut
    form changes

--- website/music/urls.py
+++ website/music/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     #/music/
     url(r'^$', views.index, name='index'),
 
-    #/music/712/
+    #/music/albumid/
     url(r'^(?P<album_id>[0-9]+)/$', views.details,name='detail'),
+    #/music/albumid/favourite/
+    url(r'^(?P<album_id>[0-9]+)/favourite/$', views.favourite,name='favourite'),
 ]
--- website/music/views.py
+++ website/music/views.py
@@ -1,6 +1,6 @@
 from django.http import Http404
 from django.http import HttpResponse
-from .models import  Album
+from .models import  Album,Song
 #from django.template import loader
 from django.shortcuts import render, get_object_or_404
 """
@@ -27,3 +27,18 @@ def details(request,album_id):
     #return HttpResponse("<h2>Details for Album id:" + str(album_id)+"</h2>")
     album=get_object_or_404(Album,pk=album_id)
     return render(request, 'music/detail.html', {'album':album})
+
+def favourite(request,album_id):
+    #return HttpResponse("<h2>Details for Album id:" + str(album_id)+"</h2>")
+    album=get_object_or_404(Album,pk=album_id)
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+    except (KeyError, Song.DoesNotExist):
+            return render(request, 'music/detail.html', {
+                'album': album,
+                'error_message': "You did not select a valid song"
+            })
+    else:
+        selected_song.is_favourite= True
+        selected_song.save()
+        return render(request, 'music/detail.html', {'album':album})
